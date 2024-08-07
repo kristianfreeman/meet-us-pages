a4188a209b1c8c1e3c1440d0d4f1c3b397754be6
Craig Dennis <craigsdennis@gmail.com>
2024-08-07 14:32:48 -0700

    Whoops on hono link

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ app.get('/', (c) => {
         <ul class="links-list">
             <li>We'd love to chat at <a href="https://itrevolution.com/product/enterprise-technology-leadership-summit-las-vegas-2024/?utm_medium=cpc&utm_source=google&utm_campaign=itrev2024&gad_source=1&gclid=CjwKCAjwm_SzBhAsEiwAXE2Cv56GG5J-ALHMYBOLmJKNy2V8IiYTXbESXyeWQd7jYY0flL1jmCxR5BoCzx8QAvD_BwE">Enterprise Technology Leadership Summit | Las Vegas, NV | August 20-22</a></li>
             <li>Swing by the <a href="https://aihwedgesummit.com/events/aihwedgesummit?gad_source=1&gclid=CjwKCAjwm_SzBhAsEiwAXE2Cv9lqbPs0CbV2ihYXB-zGaZ7XVbtnHa2NxISzBM2klcilzyyRmJ1O-RoC410QAvD_BwE">AI Hardware & Edge AI Summit | San Jose, CA | September 9-12</a></li>
-            <li>Pop over to <a href="https://www.smalldatasf.com/">Small Data | San Francisco, CA | September 24th</a></li>
+            <li>Come say hi at <a href="https://www.smalldatasf.com/">Small Data | San Francisco, CA | September 24th</a></li>
         </ul>
         <h2>Online</h2>
         <ul class="links-list">
@@ -41,7 +41,7 @@ app.get('/', (c) => {
 </main>
 <footer>
     <div class="container">
-        <p>Built with 🧡 on Cloudflare <a href="https://pages.cloudflare.com">Pages</a> w/ <a href="honojs.dev">Hono</a> 🔥</p>
+        <p>Built with 🧡 on Cloudflare <a href="https://pages.cloudflare.com">Pages</a> w/ <a href="https://honojs.dev">Hono</a> 🔥</p>
     </div>
 </footer>
 </>);
