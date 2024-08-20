b8b8e5b5cb563f8140d205c5d9a764853c556bb6
Craig Dennis <craigsdennis@gmail.com>
2024-08-20 07:52:21 -0700

    Adds Hacknight

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,7 @@ app.get('/', (c) => {
         <h2>In Person</h2>
         <ul class="links-list">
             <li>We'd love to chat at <a href="https://itrevolution.com/product/enterprise-technology-leadership-summit-las-vegas-2024/?utm_medium=cpc&utm_source=google&utm_campaign=itrev2024&gad_source=1&gclid=CjwKCAjwm_SzBhAsEiwAXE2Cv56GG5J-ALHMYBOLmJKNy2V8IiYTXbESXyeWQd7jYY0flL1jmCxR5BoCzx8QAvD_BwE">Enterprise Technology Leadership Summit | Las Vegas, NV | August 20-22</a></li>
+            <li>Hack with us at <a href="https://lu.ma/dhbn6hf3">Las Vegas AI Hacknight</a></li>
             <li>Swing by the <a href="https://aihwedgesummit.com/events/aihwedgesummit?gad_source=1&gclid=CjwKCAjwm_SzBhAsEiwAXE2Cv9lqbPs0CbV2ihYXB-zGaZ7XVbtnHa2NxISzBM2klcilzyyRmJ1O-RoC410QAvD_BwE">AI Hardware & Edge AI Summit | San Jose, CA | September 9-12</a></li>
             <li>Come say hi at <a href="https://www.smalldatasf.com/">Small Data | San Francisco, CA | September 24th</a></li>
         </ul>
